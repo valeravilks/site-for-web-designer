462b6f30bd92bec91bbbd66c37e88691e140287b
Валера <valeravilks@gmail.com>
2018-10-25 22:17:57 +0300

    Header desktop complete

--- src/entry/index.js
+++ src/entry/index.js
@@ -1,2 +1,20 @@
 import '../less/style.less';
+import $ from 'jquery';
+
+$(document).ready(function(){
+
+    // active menu item
+    let actionMenu = $('.active_page');
+    actionMenu.children().children().show();
+
+    // menu circle item hover show/hide
+    $('.header .menu a').hover(function(e){
+        let eventObject = $(e.target);
+        if(!eventObject.parent().hasClass('active_page')){
+            eventObject.children().toggle();
+        }
+    });
+
+
+});
 
